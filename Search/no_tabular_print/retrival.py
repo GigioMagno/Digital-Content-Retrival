06a937318053761ce99772895e5e44eb71163135
Svitol <97558303+GigioMagno@users.noreply.github.com>
2024-05-08 23:14:59 +0200

    Update retrival.py

--- Search/no_tabular_print/retrival.py
+++ Search/no_tabular_print/retrival.py
@@ -74,7 +74,7 @@ def search_name_and_occurrences(db_connection, keyword, batch_size):
         if not out:
             break
         for _list_ in out:
-            print(f'%s\t%s\t%d' % (_list_[0], _list_[1], _list_[2]))
+            print('%s\t%s\t%d' % (_list_[0], _list_[1], _list_[2]))
 
     db_reference.close()
     #query che ritorna il le tuple che hanno occorrenza nel nome
