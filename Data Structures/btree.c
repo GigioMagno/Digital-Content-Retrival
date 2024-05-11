d0f0ec256d909286602669490cb3cc2ca2ff05b4
Svitol <97558303+GigioMagno@users.noreply.github.com>
2024-05-11 09:57:26 +0200

    Update btree.c

--- Data Structures/btree.c
+++ Data Structures/btree.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 struct node
 {
 	char *searchKey;
@@ -11,7 +10,6 @@ struct node
 };
 
 typedef struct node node;
-
 
 node *create_node(char *searchKey){
     node *n = malloc(sizeof(node)); // Alloca memoria per il nuovo nodo
@@ -36,8 +34,6 @@ void add_node(node **root, node *newnode){
         return;
     }
 }
-
-
 
 void print_tree(node *root);
 void print_tree(node *root){
@@ -67,37 +63,6 @@ node *get_minimum(node **root){
 		current = current->left;
 	return current;
 }
-
-
-// void check_delete(node **parent, node **child, char *searchKey);
-// void check_delete(node **parent, node **child, char *searchKey){
-
-// 	node *_parent_ = *parent;
-// 	node *_child_ = *child;
-
-// 	if (_child_->right != NULL && _child_->left == NULL)
-// 	{
-// 		_parent_->right = _child_->right;
-// 		free(child);
-// 	} else if (_child_->right == NULL && _child_->left != NULL)
-// 	{
-// 		_parent_->left = _child_->left;
-// 		free(child);
-// 	} else if (_child_->right != NULL && _child_->left != NULL)
-// 	{
-// 		node *minimum = get_minimum(&(_child_->right));
-// 		minimum->left = _child_->left;
-// 		_child_->right->left = minimum->right;
-// 		minimum->right = _child_->right;
-// 		free(_child_);
-// 		_parent_->left = minimum;
-// 	}
-// 	//non sono alla fine dell'albero, ma sono all'interno.
-// }
-
-//strcmp(_child_->searchKey,searchKey)==0 
-	
-
 
 void delete_node(node **root, char* searchKey);
 void delete_node(node **root, char* searchKey) {
@@ -137,38 +102,4 @@ void delete_node(node **root, char* searchKey) {
             free(successor); // Libera la memoria del successore
         }
     }
-}
-
-
-
-
-
-int main(int argc, char const *argv[]){
-    node *root = NULL;
-    add_node(&root, create_node("ciao"));
-    add_node(&root, create_node("ciaooo"));
-    add_node(&root, create_node("a"));
-    add_node(&root, create_node("b"));
-    add_node(&root, create_node("c"));
-    add_node(&root, create_node("d"));
-    add_node(&root, create_node("co"));
-    add_node(&root, create_node("due"));
-    add_node(&root, create_node("dodo"));
-    add_node(&root, create_node("dino"));
-    add_node(&root, create_node("aino"));
-    add_node(&root, create_node("duedue"));
-    add_node(&root, create_node("carciofo"));
-    print_tree(root);
-    printf("////////////\n");
-    // delete_node(&root, "ciao");
-    // delete_node(&root, "co");
-    delete_node(&root, "due");
-    // delete_node(&root, "dodo");
-    // delete_node(&root, "dino");
-    // delete_node(&root, "aino");
-    // delete_node(&root, "c");
-    // delete_node(&root, "D");
-    //delete_node(&root, "d");
-    print_tree(root);
-    return 0;
 }
